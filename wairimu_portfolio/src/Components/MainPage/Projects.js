df0c47e46d024d852f7d7af031dc6fe18845f626
Mercy Wairimu <105119031+MasyWairimu@users.noreply.github.com>
2024-09-18 10:07:57 +0300

    deloying the wairimu-portfolio

--- wairimu_portfolio/src/Components/MainPage/Projects.js
+++ wairimu_portfolio/src/Components/MainPage/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import FooterLine from '../../assets/FooterLine.png'
+import SafariScreenshot from '../../assets/laptop-screen1.jpeg'
 
 const Projects = () => {
     return (
@@ -11,7 +12,13 @@ const Projects = () => {
             <div className='project'>
                 <div className='projectName'>SAFARI PROJECT</div>
                 <div className='projectContent'>
-                The Safari.Travel website is a front-end project that showcases different safari destinations across Kenya. As my first React project, I focused on creating a responsive user interface using HTML, CSS/SCSS, JavaScript, and React. The project highlights my ability to design for various screen sizes, ensuring a seamless experience across devices
+                    The Safari.Travel website is a front-end project that showcases different safari destinations<br />
+                    across Kenya. As my first React project, I focused on creating a responsive user interface <br />
+                    using HTML, CSS/SCSS, JavaScript, and React. The project highlights my ability to design for <br />
+                    various screen sizes, ensuring a seamless experience across devices
+                </div>
+                <div className='projectImage'>
+                    <img src={SafariScreenshot} alt='Line' style={{ width: '250px' }} />
                 </div>
             </div>
             <div className='project'>
